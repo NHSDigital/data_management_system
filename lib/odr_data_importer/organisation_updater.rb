4bb530dc05e766cfe4333ba9937b056783dfcbf9
kennylee <kennylee@nhs.net>
2020-07-27 09:47:18 +0100

    # ODR importer - Organisation importer and updater tweaks - optional output to debug data issues on import

--- lib/odr_data_importer/organisation_updater.rb
+++ lib/odr_data_importer/organisation_updater.rb
@@ -5,17 +5,17 @@ module OdrDataImporter
 
       organisations_updated = 0
       not_found = []
+      log_to_process_count(@excel_file.count)
+
       @excel_file.each do |current_name, new_name|
         org = Organisation.where('name ILIKE ?', current_name.strip)
 
         if org.nil?
           not_found << current_name
-          next
         else
           org.update(name: new_name)
+          organisations_updated += 1
         end
-
-        organisations_updated += 1
       end
 
       puts "Unable to find these organisations: #{not_found.join(', ')}" if not_found.present?
