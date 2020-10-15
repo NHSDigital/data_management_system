4386c75cf19bc1ce78511330f1eacc105ab696ff
Francesco <francesco.santaniello@phe.gov.uk>
2020-10-15 20:16:52 +0100

    rubocopped RP4 germline colorectal importer coverage test

--- test/lib/import/colorectal/providers/london_gosh/london_gosh_handler_colorectal_test.rb
+++ test/lib/import/colorectal/providers/london_gosh/london_gosh_handler_colorectal_test.rb
@@ -16,49 +16,49 @@ class LondonGoshHandlerColorectalTest < ActiveSupport::TestCase
   end
 
   test 'process_gene_and_variant' do
-  @logger.expects(:debug).with("NO MUTATION FOUND")
-  @logger.expects(:debug).with('Negative genes are ["EPCAM", "MLH1", "MSH2", "MSH6"]')
-  @logger.expects(:debug).with("SUCCESSFUL gene parse for EPCAM")
-  @logger.expects(:debug).with("SUCCESSFUL gene parse for MLH1")
-  @logger.expects(:debug).with("SUCCESSFUL gene parse for MSH2")
-  @logger.expects(:debug).with("SUCCESSFUL gene parse for MSH6")
-  assert_equal 4, @handler.process_gene_and_variant(@genotype, @record).size
-  cdna_record = build_raw_record('pseudo_id1' => 'bob')
-  cdna_record.raw_fields["gene"] = "MLH1"
-  cdna_record.raw_fields["acmg_cdna"] = "NM_000179.2:c.2960C>T"
-  cdna_record.raw_fields["acmg_protein_change"] = "p.Thr987Ile"
-  @logger.expects(:debug).with("Found mutated gene MLH1")
-  @logger.expects(:debug).with('Negative genes are ["EPCAM", "MSH2", "MSH6"]')
-  @logger.expects(:debug).with("SUCCESSFUL gene parse for EPCAM")
-  @logger.expects(:debug).with("SUCCESSFUL gene parse for MSH2")
-  @logger.expects(:debug).with("SUCCESSFUL gene parse for MSH6")
-  @logger.expects(:debug).with("SUCCESSFUL gene parse for MLH1")
-  assert_equal 4, @handler.process_gene_and_variant(@genotype, cdna_record).size
+    @logger.expects(:debug).with('NO MUTATION FOUND')
+    @logger.expects(:debug).with('Negative genes are ["EPCAM", "MLH1", "MSH2", "MSH6"]')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for EPCAM')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MLH1')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH2')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH6')
+    assert_equal 4, @handler.process_gene_and_variant(@genotype, @record).size
+    cdna_record = build_raw_record('pseudo_id1' => 'bob')
+    cdna_record.raw_fields['gene'] = 'MLH1'
+    cdna_record.raw_fields['acmg_cdna'] = 'NM_000179.2:c.2960C>T'
+    cdna_record.raw_fields['acmg_protein_change'] = 'p.Thr987Ile'
+    @logger.expects(:debug).with('Found mutated gene MLH1')
+    @logger.expects(:debug).with('Negative genes are ["EPCAM", "MSH2", "MSH6"]')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for EPCAM')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH2')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH6')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MLH1')
+    assert_equal 4, @handler.process_gene_and_variant(@genotype, cdna_record).size
   end
-  
+
   test 'coding_variant' do
     cdna_record = build_raw_record('pseudo_id1' => 'bob')
-    cdna_record.raw_fields["gene"] = "MLH1"
-    cdna_record.raw_fields["acmg_cdna"] = "NM_000179.2:c.2960C>T"
-    cdna_record.raw_fields["acmg_protein_change"] = "p.Val717Alafs*18"
+    cdna_record.raw_fields['gene'] = 'MLH1'
+    cdna_record.raw_fields['acmg_cdna'] = 'NM_000179.2:c.2960C>T'
+    cdna_record.raw_fields['acmg_protein_change'] = 'p.Val717Alafs*18'
     genotypes = []
     mutated_gene = cdna_record.raw_fields['gene']
     all_genes = cdna_record.raw_fields['genes analysed'] unless cdna_record.raw_fields['genes analysed'].nil?
     dna_variant = cdna_record.raw_fields['acmg_cdna'] unless cdna_record.raw_fields['acmg_cdna'].nil?
     protein_variant = cdna_record.raw_fields['acmg_protein_change']
-    @logger.expects(:debug).with("Found mutated gene MLH1")
+    @logger.expects(:debug).with('Found mutated gene MLH1')
     @logger.expects(:debug).with('Negative genes are ["EPCAM", "MSH2", "MSH6"]')
-    @logger.expects(:debug).with("SUCCESSFUL gene parse for EPCAM")
-    @logger.expects(:debug).with("SUCCESSFUL gene parse for MSH2")
-    @logger.expects(:debug).with("SUCCESSFUL gene parse for MSH6")
-    @logger.expects(:debug).with("SUCCESSFUL gene parse for MLH1")
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for EPCAM')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH2')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH6')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for MLH1')
     assert_equal 4, @handler.coding_variant(@genotype, genotypes, mutated_gene, dna_variant, protein_variant, all_genes).size
   end
 
   test 'exonic_variant' do
     exon_record = build_raw_record('pseudo_id1' => 'bob')
-    exon_record.raw_fields["gene"] = "MLH1"
-    exon_record.raw_fields["codingdnasequencechange"] = "MSH2 exon 11-16 deletion"
+    exon_record.raw_fields['gene'] = 'MLH1'
+    exon_record.raw_fields['codingdnasequencechange'] = 'MSH2 exon 11-16 deletion'
     genotypes = []
     mutated_gene = exon_record.raw_fields['gene']
     all_genes = exon_record.raw_fields['genes analysed'] unless exon_record.raw_fields['genes analysed'].nil?
@@ -76,8 +76,8 @@ class LondonGoshHandlerColorectalTest < ActiveSupport::TestCase
 
   test 'negative_mutated_genes' do
     negative_record = build_raw_record('pseudo_id1' => 'bob')
-    negative_record.raw_fields["gene"] = "MLH1"
-    negative_record.raw_fields["codingdnasequencechange"] = "MSH2 exon 11-16 deletion"
+    negative_record.raw_fields['gene'] = 'MLH1'
+    negative_record.raw_fields['codingdnasequencechange'] = 'MSH2 exon 11-16 deletion'
     genotypes = []
     mutated_gene = negative_record.raw_fields['gene']
     all_genes = negative_record.raw_fields['genes analysed'] unless negative_record.raw_fields['genes analysed'].nil?
@@ -113,20 +113,18 @@ class LondonGoshHandlerColorectalTest < ActiveSupport::TestCase
       age: 999 }.to_json
   end
 
-
-
   def rawtext_clinical_json
-    { "lab no." => 'Laboratory',
-      "family id" => '0000000',
+    { 'lab no.' => 'Laboratory',
+      'family id' => '0000000',
       requesteddate: '2018-12-18 00:00:00',
       providercode: 'Provider Code',
       genetictestscope: 'COLORECTAL CANCER REPORT',
       collecteddate: '2012-06-28 00:00:00',
       receiveddate: '2012-06-29 00:00:00',
       authoriseddate: '2019-04-19 00:00:00',
-      "sample in goshg2p" => 'xxxxxx',
+      'sample in goshg2p' => 'xxxxxx',
       sex: 'F',
-      "genes analysed" => 'EPCAM;MLH1;MSH2;MSH6',
+      'genes analysed' => 'EPCAM;MLH1;MSH2;MSH6',
       gene: '',
       codingdnasequencechange: '',
       proteinimpact: '',
