6bafda39c192aaa549e2d0dc9bb783db10c24a95
Joe <joseph.catling@phe.gov.uk>
2020-11-18 16:30:37 +0000

    migration fix

--- db/migrate/20201118154616_add_cas_workflow.rb
+++ db/migrate/20201118154616_add_cas_workflow.rb
@@ -9,13 +9,7 @@ class AddCasWorkflow < ActiveRecord::Migration[6.0]
 
   def change
     project_type = ProjectType.find_by!(name: 'CAS')
-    (1..2).each.with_index(current_max_id) do |_, i|
-      add_lookup Transition, i, from_state_id: 'DRAFT', next_state_id: 'SUBMITTED', project_type: project_type
-      add_lookup Transition, i, from_state_id: 'DRAFT', next_state_id: 'DELETED', project_type: project_type
-    end
-  end
-
-  def current_max_id
-    Transition.maximum(:id)
+    add_lookup Transition, 76, from_state_id: 'DRAFT', next_state_id: 'SUBMITTED', project_type: project_type
+    add_lookup Transition, 77, from_state_id: 'DRAFT', next_state_id: 'DELETED', project_type: project_type
   end
 end
