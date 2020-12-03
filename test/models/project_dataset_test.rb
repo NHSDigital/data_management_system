cb5a4a239bec1e659410aed29b02c48e77ce7bf5
Joe <joseph.catling@phe.gov.uk>
2020-12-03 17:19:23 +0000

    cas dataset auto-transition test

--- test/models/project_dataset_test.rb
+++ test/models/project_dataset_test.rb
@@ -38,5 +38,59 @@ class ProjectDatasetTest < ActiveSupport::TestCase
     project_dataset.save!
 
     assert_equal 0, ProjectDataset.outstanding_approval(users(:cas_dataset_approver)).count
+  test 'dataset_approval scope should only return approved status is nil if passed that argument' do
+    project = Project.create(project_type: project_types(:cas), owner: users(:standard_user2))
+    project_dataset = ProjectDataset.new(dataset: Dataset.find_by(name: 'Extra CAS Dataset One'),
+                                         approved: nil)
+    project.project_datasets << project_dataset
+
+    assert_equal 1, ProjectDataset.dataset_approval(users(:cas_dataset_approver), nil).count
+
+    project_dataset.approved = true
+    project_dataset.save!
+
+    assert_equal 0, ProjectDataset.dataset_approval(users(:cas_dataset_approver), nil).count
+  end
+
+  test 'should only auto-transition cas application if there are no unresolved dataset decisions' do
+    application = Project.new(project_type: ProjectType.find_by(name: 'CAS')).tap do |a|
+      a.owner = users(:no_roles)
+      a.project_datasets << ProjectDataset.new(dataset: dataset(83), terms_accepted: true,
+                                               approved: nil)
+      a.project_datasets << ProjectDataset.new(dataset: dataset(84), terms_accepted: true,
+                                               approved: nil)
+      a.transition_to!(workflow_states(:submitted))
+      a.save!
+    end
+
+    refute_equal application.current_state, workflow_states(:awaiting_account_approval)
+
+    application.project_datasets.first.update(approved: true)
+
+    refute_equal application.current_state, workflow_states(:awaiting_account_approval)
+
+    application.project_datasets.last.update(approved: false)
+
+    assert_equal application.current_state, workflow_states(:awaiting_account_approval)
+
+    application.project_datasets.last.update(approved: true)
+
+    assert_equal 1, application.states.where(id: "AWAITING_ACCOUNT_APPROVAL").count
+  end
+
+  test 'should not auto-transition cas application if not at submitted state' do
+    application = Project.new(project_type: ProjectType.find_by(name: 'CAS')).tap do |a|
+      a.owner = users(:no_roles)
+      a.project_datasets << ProjectDataset.new(dataset: dataset(83), terms_accepted: true,
+                                               approved: nil)
+      a.project_datasets << ProjectDataset.new(dataset: dataset(84), terms_accepted: true,
+                                               approved: nil)
+      a.save!
+    end
+
+    application.project_datasets.first.update(approved: true)
+    application.project_datasets.last.update(approved: false)
+
+    refute_equal application.current_state, workflow_states(:awaiting_account_approval)
   end
 end
