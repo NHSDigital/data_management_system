6ba35994820a2264af94b327c2dbe60710e40a15
Francesco <francesco.santaniello@phe.gov.uk>
2020-12-15 08:52:35 +0000

    #Edited Birmingham handler in order to retrieve constants from helper file

--- lib/import/colorectal/providers/birmingham/birmingham_handler_colorectal.rb
+++ lib/import/colorectal/providers/birmingham/birmingham_handler_colorectal.rb
@@ -2,6 +2,7 @@ require 'possibly'
 require 'import/storage_manager/persister'
 require 'pry'
 require 'import/brca/core/provider_handler'
+require 'import/helpers/colorectal/providers/rq3/rq3_constants'
 
 module Import
   module Colorectal
@@ -9,9 +10,7 @@ module Import
       module Birmingham
         # Process Cambridge-specific record details into generalized internal genotype format
         class BirminghamHandlerColorectal < Import::Brca::Core::ProviderHandler
-          PASS_THROUGH_FIELDS_COLO = %w[age sex consultantcode servicereportidentifier providercode
-                                        authoriseddate receiveddate moleculartestingtype
-                                        specimentype].freeze
+          include Import::Helpers::Colorectal::Providers::Rq3::Rq3Constants
 
           def initialize(batch)
             @failed_genocolorectal_counter = 0
@@ -22,52 +21,33 @@ module Import
             super
           end
 
-          TEST_SCOPE_MAP_COLO_COLO = { '100kgp confirmation'  => :full_screen,
-                                       'carrier testing'      => :targeted_mutation,
-                                       'confirmation'         => :targeted_mutation,
-                                       'diagnosis'            => :full_screen,
-                                       'family studies'       => :targeted_mutation,
-                                       'follow-up'            => :targeted_mutation,
-                                       'indirect testing'     => :full_screen,
-                                       'pold1/ pole analysis' => :full_screen,
-                                       'prenatal diagnosis'   => :targeted_mutation,
-                                       'presymptomatic'       => :targeted_mutation }.freeze
-                                       
-          COLORECTAL_GENES_REGEX = /(?<colorectal>APC|
-                                                BMPR1A|
-                                                EPCAM|
-                                                MLH1|
-                                                MSH2|
-                                                MSH6|
-                                                MUTYH|
-                                                PMS2|
-                                                POLD1|
-                                                POLE|
-                                                PTEN|
-                                                SMAD4|
-                                                STK11|
-                                                NTHL1)/xi . freeze # Added by Francesco
-
-          CDNA_REGEX_COLO = /c\.(?<cdna>.*)/i.freeze
-          PROTEIN_REGEX_COLO = /p.(?:\((?<impact>.*)\))/.freeze
-          EXON_LOCATION_REGEX_COLO = /exons? (\d+[a-z]*(?: ?- ?\d+[a-z]*)?)/i.freeze
-
           def process_fields(record)
             genocolorectal = Import::Colorectal::Core::Genocolorectal.new(record)
             genocolorectal.add_passthrough_fields(record.mapped_fields,
                                                   record.raw_fields,
                                                   PASS_THROUGH_FIELDS_COLO)
             process_genetictestscope(genocolorectal, record)
+            teststatus_report_inconsistency(genocolorectal, record)
             @persister.integrate_and_store(genocolorectal)
           end
 
           def process_genetictestscope(genocolorectal, record)
-            if record.raw_fields['moleculartestingtype']
-              tscope = record.raw_fields['moleculartestingtype']
-              genocolorectal.add_test_scope(TEST_SCOPE_MAP_COLO_COLO[tscope.downcase.strip])
-              @logger.debug 'Processed genetictestscope'\
-                            "#{TEST_SCOPE_MAP_COLO_COLO[tscope.downcase.strip]} for #{tscope}"
-            else @logger.debug 'UNABLE to process genetictestscope'
+            Maybe(record.raw_fields['moleculartestingtype']).each do |tscope|
+              if TEST_SCOPE_MAP_COLO_COLO[tscope.downcase.strip]
+                genocolorectal.add_test_scope(TEST_SCOPE_MAP_COLO_COLO[tscope.downcase.strip])
+                @logger.debug 'Processed genetictestscope '\
+                              "#{TEST_SCOPE_MAP_COLO_COLO[tscope.downcase.strip]} for #{tscope}"
+              else @logger.debug 'UNABLE to process genetictestscope'
+              end
+            end
+          end
+
+          def teststatus_report_inconsistency(genocolorectal, record)
+            if COLORECTAL_GENES_MAP[record.raw_fields['indication']] &&
+              record.raw_fields['teststatus'].scan(COLORECTAL_GENES_REGEX).size > 0 && 
+              record.raw_fields['report'].scan(COLORECTAL_GENES_REGEX).size > 0 &&
+               record.raw_fields['teststatus'].scan(COLORECTAL_GENES_REGEX).uniq.size != record.raw_fields['report'].scan(COLORECTAL_GENES_REGEX).uniq.size
+              binding.pry
             end
           end
 
