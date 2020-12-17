39ae45b49b7db5cd93320e1f5077561ca7fa18f6
Francesco <francesco.santaniello@phe.gov.uk>
2020-12-17 12:24:59 +0000

    #Added processing method for negative tests in RQ3 colorectal handler

--- lib/import/colorectal/providers/birmingham/birmingham_handler_colorectal.rb
+++ lib/import/colorectal/providers/birmingham/birmingham_handler_colorectal.rb
@@ -28,27 +28,23 @@ module Import
                                                   record.raw_fields,
                                                   PASS_THROUGH_FIELDS_COLO)
             process_genetictestscope(genocolorectal, record)
-            teststatus_report_inconsistency(genocolorectal, record)
-            @persister.integrate_and_store(genocolorectal)
+            res = process_variants_from_report(genocolorectal, record)
+            res.each { |cur_genotype| @persister.integrate_and_store(cur_genotype) }
+            # @persister.integrate_and_store(genocolorectal)
           end
 
           def process_genetictestscope(genocolorectal, record)
             Maybe(record.raw_fields['moleculartestingtype']).each do |tscope|
-              @test_number = @test_number + 1
-              @logger
               if TEST_SCOPE_MAP_COLO_COLO[tscope.downcase.strip]
                 genocolorectal.add_test_scope(TEST_SCOPE_MAP_COLO_COLO[tscope.downcase.strip])
                 @logger.debug 'Processed genetictestscope '\
                               "#{TEST_SCOPE_MAP_COLO_COLO[tscope.downcase.strip]} for #{tscope}"
               else @logger.debug 'UNABLE to process genetictestscope'
               end
-              @logger.debug "Test number #{@test_number}"
             end
           end
 
-          def teststatus_report_inconsistency(genocolorectal, record)
-            # @test_number = @test_number + 1
-            # @logger.debug "Test number #{@test_number}"
+          def process_variants_from_report(genocolorectal, record)
             genotypes = []
             posnegtest = record.raw_fields['overall2']
             testresult = record.raw_fields['teststatus']
@@ -56,44 +52,50 @@ module Import
             #TO DO: WRITE FUNCTIONS FOR GENOTYPE LIST POPULATION AND REDUCE FUNCTIONS TO
             # NON-REDUNDANT METHODS
             if COLORECTAL_GENES_MAP[record.raw_fields['indication']]
+              genelist = COLORECTAL_GENES_MAP[record.raw_fields['indication']]
               if posnegtest.upcase == 'P' # if there is an abnormal test
-                @logger.debug 'ABNORMAL TEST FOUND'
-                if testresult.match(CDNA_REGEX)
-                  @logger.debug 'Found CDNA variant'
-                  if testresult.nil? || testresult.scan(COLORECTAL_GENES_REGEX).empty?
-                    @logger.debug 'TEST RESULTS EMPTY'
-                  elsif testreport.nil? || testreport.scan(COLORECTAL_GENES_REGEX).empty?
-                    @logger.debug 'TEST REPORT EMPTY'
-                  elsif !testresult.scan(COLORECTAL_GENES_REGEX).empty? && !testreport.scan(COLORECTAL_GENES_REGEX).empty? && 
-                    testresult.scan(COLORECTAL_GENES_REGEX).uniq.size != testreport.scan(COLORECTAL_GENES_REGEX).uniq.size
-                      @logger.debug 'INCONSISTENCY OF DECLARED GENES IN TESTRESULT AND TESTREPORT'
-                  elsif !testresult.scan(COLORECTAL_GENES_REGEX).empty? && !testreport.scan(COLORECTAL_GENES_REGEX).empty? && 
-                    testresult.scan(COLORECTAL_GENES_REGEX).uniq.size == testreport.scan(COLORECTAL_GENES_REGEX).uniq.size
-                    @logger.debug 'SAME NUMBER OF DECLARED GENES IN TESTRESULT AND TESTREPORT'
-                    end
-                elsif testresult.match(CHR_VARIANTS_REGEX)
-                  @logger.debug 'LARGE CHROMOSOMAL ABERRATION FOUND'
-                elsif testresult.match(CHR_VARIANTS_REGEX) && testresult.match(CDNA_REGEX)
-                else @logger.debug 'FOUND BOTH LARGE CHROMOSOMAL ABERRATION AND CDNA VARIANT'
-                end
+                @logger.debug 'ABNORMAL TEST'
+                # @logger.debug 'ABNORMAL TEST FOUND'
+                # if testresult.match(CDNA_REGEX)
+                #   @logger.debug 'Found CDNA variant'
+                #   if testresult.nil? || testresult.scan(COLORECTAL_GENES_REGEX).empty?
+                #     @logger.debug 'TEST RESULTS EMPTY'
+                #   elsif testreport.nil? || testreport.scan(COLORECTAL_GENES_REGEX).empty?
+                #     @logger.debug 'TEST REPORT EMPTY'
+                #   elsif !testresult.scan(COLORECTAL_GENES_REGEX).empty? && !testreport.scan(COLORECTAL_GENES_REGEX).empty? &&
+                #     testresult.scan(COLORECTAL_GENES_REGEX).uniq.size != testreport.scan(COLORECTAL_GENES_REGEX).uniq.size
+                #       @logger.debug 'INCONSISTENCY OF DECLARED GENES IN TESTRESULT AND TESTREPORT'
+                #   elsif !testresult.scan(COLORECTAL_GENES_REGEX).empty? && !testreport.scan(COLORECTAL_GENES_REGEX).empty? &&
+                #     testresult.scan(COLORECTAL_GENES_REGEX).uniq.size == testreport.scan(COLORECTAL_GENES_REGEX).uniq.size
+                #     @logger.debug 'SAME NUMBER OF DECLARED GENES IN TESTRESULT AND TESTREPORT'
+                #   end
+                # elsif testresult.match(CHR_VARIANTS_REGEX)
+                #   @logger.debug 'LARGE CHROMOSOMAL ABERRATION FOUND'
+                # elsif testresult.match(CHR_VARIANTS_REGEX) && testresult.match(CDNA_REGEX)
+                # else @logger.debug 'FOUND BOTH LARGE CHROMOSOMAL ABERRATION AND CDNA VARIANT'
+                # end
               elsif posnegtest.upcase == 'N'
                 @logger.debug 'NORMAL TEST FOUND'
-                if testresult.nil? || testresult.scan(COLORECTAL_GENES_REGEX).empty?
-                  @logger.debug 'TEST RESULTS EMPTY'
-                elsif testreport.nil? || testreport.scan(COLORECTAL_GENES_REGEX).empty?
-                  @logger.debug 'TEST REPORT EMPTY'
-                elsif !testresult.scan(COLORECTAL_GENES_REGEX).empty? && !testreport.scan(COLORECTAL_GENES_REGEX).empty? && 
-                  testresult.scan(COLORECTAL_GENES_REGEX).uniq.size != testreport.scan(COLORECTAL_GENES_REGEX).uniq.size
-                    @logger.debug 'INCONSISTENCY OF DECLARED GENES IN TESTRESULT AND TESTREPORT'
-                elsif !testresult.scan(COLORECTAL_GENES_REGEX).empty? && !testreport.scan(COLORECTAL_GENES_REGEX).empty? && 
-                  testresult.scan(COLORECTAL_GENES_REGEX).uniq.size == testreport.scan(COLORECTAL_GENES_REGEX).uniq.size
-                  @logger.debug 'SAME NUMBER OF DECLARED GENES IN TESTRESULT AND TESTREPORT'
-                end
+                negativegenes = genelist
+                process_negative_genes(negativegenes, genotypes, genocolorectal, record)
               end
             end
-            @logger.debug "Test number #{@test_number}"
+            genotypes
           end
 
+          def process_negative_genes(negativegenes, genotypes, genocolorectal, record)
+            negativegenes.each do |negativegene|
+              dup_genocolorectal = genocolorectal.dup_colo
+              @logger.debug "Found #{negativegene} for list #{negativegenes}"
+              dup_genocolorectal.add_status(1)
+              dup_genocolorectal.add_gene_colorectal(negativegene)
+              dup_genocolorectal.add_protein_impact(nil)
+              dup_genocolorectal.add_gene_location(nil)
+              genotypes.append(dup_genocolorectal)
+            end
+          end
+          
+          
           def summarize
             @logger.info '***************** Handler Report *******************'
             @logger.info "Num genes failed to parse: #{@failed_gene_counter} of "\
