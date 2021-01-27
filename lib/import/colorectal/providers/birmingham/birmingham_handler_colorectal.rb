NHSDigital/data_management_system

commit 82d540410cfb04dbdbdffefa047a735f9adcd29c

# Initial commit of Birmingham handler refactoring

lib/import/colorectal/providers/birmingham/birmingham_handler_colorectal.rb
@@ -1,14 +1,10 @@
-require 'possibly'
-require 'import/storage_manager/persister'
-require 'pry'
-require 'import/brca/core/provider_handler'
 require 'import/helpers/colorectal/providers/rq3/rq3_constants'
 require 'import/helpers/colorectal/providers/rq3/rq3_helper'
 module Import
   module Colorectal
     module Providers
       module Birmingham
-        # Process Cambridge-specific record details into generalized internal genotype format
+        # Process Birmingham-specific record details into generalized internal genotype format
         class BirminghamHandlerColorectal < Import::Brca::Core::ProviderHandler
           include Import::Helpers::Colorectal::Providers::Rq3::Rq3Constants
           include Import::Helpers::Colorectal::Providers::Rq3::Rq3Helper
@@ -42,66 +38,53 @@ module Import
           end
 
           def process_variants_from_report(genocolorectal, record)
-            genotypes = []
+            genotypes  = []
             posnegtest = record.raw_fields['overall2']
             testresult = record.raw_fields['teststatus']
             testreport = record.raw_fields['report']
-            if COLORECTAL_GENES_MAP[record.raw_fields['indication']]
-              genelist = COLORECTAL_GENES_MAP[record.raw_fields['indication']]
-              if posnegtest.upcase == 'P'
-                @logger.debug 'ABNORMAL TEST'
-                if testresult.scan(/MYH/).size.positive?
-                  process_mutyh_specific_variants(testresult, genelist, genotypes, genocolorectal, record)
-                elsif testresult.scan(COLORECTAL_GENES_REGEX).empty?
-                  if testreport.scan(CDNA_REGEX).size.positive?
-                    process_testreport_cdna_variants(testreport, genelist, genotypes, genocolorectal, record)
-                  elsif testreport.scan(CHR_VARIANTS_REGEX).size.positive?
-                    testcolumn = testreport
-                    process_chromosomal_variant(testcolumn, genelist, genotypes, record, genocolorectal)
-                  else
-                    process_malformed_variants(testresult, testreport, genelist, genotypes, genocolorectal, record)
-                  end
+            genelist   = COLORECTAL_GENES_MAP[record.raw_fields['indication']]
+            return genotypes unless genelist
+
+            if posnegtest.upcase == 'P'
+              @logger.debug 'ABNORMAL TEST'
+              if testresult.scan(/MYH/).size.positive?
+                process_mutyh_specific_variants(testresult, genelist, genotypes,
+                                                genocolorectal, record)
+              elsif testresult.scan(COLORECTAL_GENES_REGEX).empty?
+                if testreport.scan(CDNA_REGEX).size.positive?
+                  process_testreport_cdna_variants(testreport, genelist, genotypes,
+                                                   genocolorectal, record)
+                elsif testreport.scan(CHR_VARIANTS_REGEX).size.positive?
+                  process_chromosomal_variant(testreport, genelist, genotypes,
+                                              record, genocolorectal)
                 else
-                  if testresult.scan(CDNA_REGEX).size.positive?
-                    process_testresult_cdna_variants(testresult, testreport, genelist, genotypes, record, genocolorectal)
-                  elsif testresult.scan(CHR_VARIANTS_REGEX).size.positive?
-                    if full_screen?(record)
-                      if sometimes_tested?(record)
-                        genelist = testreport.scan(COLORECTAL_GENES_REGEX).flatten.uniq
-                        negativegenes = genelist - testresult.scan(COLORECTAL_GENES_REGEX).flatten.uniq
-                        process_negative_genes(negativegenes, genotypes, genocolorectal)
-                      else
-                        negativegenes = genelist - testresult.scan(COLORECTAL_GENES_REGEX).flatten.uniq
-                        process_negative_genes(negativegenes, genotypes, genocolorectal)
-                      end
-                    end
-                    testcolumn = testresult
-                    process_chromosomal_variant(testcolumn, genelist, genotypes, record, genocolorectal)
-                  elsif testresult.match(/No known pathogenic/i)
-                    negativegenes = genelist
-                    process_negative_genes(negativegenes, genotypes, genocolorectal)
-                  else
-                    if full_screen?(record)
-                      if sometimes_tested?(record)
-                        genelist = testreport.scan(COLORECTAL_GENES_REGEX).flatten.uniq
-                        negativegenes = genelist - testresult.scan(COLORECTAL_GENES_REGEX).flatten.uniq
-                        process_negative_genes(negativegenes, genotypes, genocolorectal)
-                      else
-                        negativegenes = genelist - testresult.scan(COLORECTAL_GENES_REGEX).flatten.uniq
-                        process_negative_genes(negativegenes, genotypes, genocolorectal)
-                      end
-                    end
-                    genocolorectal.add_gene_colorectal(testresult.scan(COLORECTAL_GENES_REGEX).join)
-                    genocolorectal.add_gene_location('')
-                    genocolorectal.add_status(2)
-                    genotypes.append(genocolorectal)
-                  end
+                  process_malformed_variants(testresult, testreport, genelist, genotypes,
+                                             genocolorectal, record)
                 end
-              elsif posnegtest.upcase == 'N'
-                process_negative_records(genelist, genotypes, testresult, testreport, record, genocolorectal)
-              else @logger.debug "UNRECOGNISED TAG FOR #{record.raw_fields[indication]}"
+              elsif testresult.scan(CDNA_REGEX).size.positive?
+                process_testresult_cdna_variants(testresult, testreport, genelist,
+                                                 genotypes, record, genocolorectal)
+              elsif testresult.scan(CHR_VARIANTS_REGEX).size.positive?
+                process_chr_variants(record, testreport, genotypes, genocolorectal)
+              elsif testresult.match(/No known pathogenic/i)
+                negativegenes = genelist
+                process_negative_genes(negativegenes, genotypes, genocolorectal)
+              else
+                if full_screen?(record)
+                  process_full_screen(record, testreport, genotypes, genocolorectal)
+                end
+                genocolorectal.add_gene_colorectal(testresult.scan(COLORECTAL_GENES_REGEX).join)
+                genocolorectal.add_gene_location('')
+                genocolorectal.add_status(2)
+                genotypes.append(genocolorectal)
               end
+            elsif posnegtest.upcase == 'N'
+              process_negative_records(genelist, genotypes, testresult,
+                                       testreport, record, genocolorectal)
+            else
+              @logger.debug "UNRECOGNISED TAG FOR #{record.raw_fields[indication]}"
             end
+
             genotypes
           end
 

lib/import/helpers/colorectal/providers/rq3/rq3_helper.rb
@@ -138,6 +138,32 @@ module Import
               end
             end
 
+            def process_chr_variants(record, testreport, genotypes, genocolorectal)
+              if full_screen?(record)
+                if sometimes_tested?(record)
+                  genelist = testreport.scan(COLORECTAL_GENES_REGEX).flatten.uniq
+                  negativegenes = genelist - testresult.scan(COLORECTAL_GENES_REGEX).flatten.uniq
+                  process_negative_genes(negativegenes, genotypes, genocolorectal)
+                else
+                  negativegenes = genelist - testresult.scan(COLORECTAL_GENES_REGEX).flatten.uniq
+                  process_negative_genes(negativegenes, genotypes, genocolorectal)
+                end
+              end
+              testcolumn = testresult
+              process_chromosomal_variant(testcolumn, genelist, genotypes, record, genocolorectal)
+            end
+
+            def process_full_screen(record, testreport, genotypes, genocolorectal)
+              if sometimes_tested?(record)
+                genelist = testreport.scan(COLORECTAL_GENES_REGEX).flatten.uniq
+                negativegenes = genelist - testresult.scan(COLORECTAL_GENES_REGEX).flatten.uniq
+                process_negative_genes(negativegenes, genotypes, genocolorectal)
+              else
+                negativegenes = genelist - testresult.scan(COLORECTAL_GENES_REGEX).flatten.uniq
+                process_negative_genes(negativegenes, genotypes, genocolorectal)
+              end
+            end
+
             def process_chromosomal_variant(testcolumn, genelist, genotypes, record, genocolorectal)
               if testcolumn.scan(COLORECTAL_GENES_REGEX).uniq.size == 1
                 if testcolumn.scan(CHR_VARIANTS_REGEX).size == 1
@@ -166,7 +192,8 @@ module Import
               end
             end
 
-            def process_negative_records(genelist, genotypes, testresult, testreport, record, genocolorectal)
+            def process_negative_records(genelist, genotypes, testresult,
+                                         testreport, record, genocolorectal)
               @logger.debug 'NORMAL TEST FOUND'
               if full_screen?(record)
                 if sometimes_tested?(record)
@@ -187,7 +214,8 @@ module Import
               end
             end
 
-            def process_testresult_single_cdnavariant(testresult, testreport, record, genelist, genotypes, genocolorectal)
+            def process_testresult_single_cdnavariant(testresult, testreport, record,
+                                                      genelist, genotypes, genocolorectal)
               if testresult.scan(COLORECTAL_GENES_REGEX).uniq.size == 1
                 if full_screen?(record)
                   if sometimes_tested?(record)
@@ -234,7 +262,8 @@ module Import
                 record.raw_fields['indication'] == 'POLY'
             end
 
-            def process_testresult_multiple_cdnavariant(testresult, testreport, record, genelist, genotypes, genocolorectal)
+            def process_testresult_multiple_cdnavariant(testresult, testreport, record,
+                                                        genelist, genotypes, genocolorectal)
               if testresult.scan(COLORECTAL_GENES_REGEX).uniq.size == 2
                 if full_screen?(record)
                   if sometimes_tested?(record)

lib/import/helpers/colorectal/providers/rq3/rq3_constants.rb
@@ -27,8 +27,9 @@ module Import
                                      'COCA' => %w[MLH1 MSH2],
                                      'POLY' => %w[APC MUTYH],
                                      'NGS_COLON' => %w[MLH1 MSH2 MSH6 APC MUTYH],
-                                     'COLON' => %w[MLH1 MSH2 MSH6 PMS2 APC MUTYH PTEN SMAD4 BMPR1A STK11],
-                                     'HNPCC' => %w[MLH1 MSH2 MSH6 PMS2 EPCAM] } .freeze
+                                     'COLON' => %w[MLH1 MSH2 MSH6 PMS2 APC MUTYH PTEN
+                                                   SMAD4 BMPR1A STK11],
+                                     'HNPCC' => %w[MLH1 MSH2 MSH6 PMS2 EPCAM] }.freeze
 
             COLORECTAL_GENES_REGEX = /(?<colorectal>APC|
                                                   BMPR1A|
@@ -43,14 +44,15 @@ module Import
                                                   PTEN|
                                                   SMAD4|
                                                   STK11|
-                                                  TACSTD1)/xi .freeze
+                                                  TACSTD1)/xi.freeze
 
-            CDNA_REGEX = /c\.(?<cdna>([0-9]+[^[:alnum:]][0-9][^[:alnum:]][0-9]+[^[:alnum:]][0-9][a-z]+)|
+            CDNA_REGEX = /c\.(?<cdna>([0-9]+[^[:alnum:]][0-9][^[:alnum:]][0-9]+
+                          [^[:alnum:]][0-9][a-z]+)|
                           ([0-9]+[a-z]+[^[:alnum:]][a-z])|
                           ([0-9]+[^[:alnum:]][0-9]+[a-z]+[^[:alnum:]][a-z])|
                           ([0-9]+[a-z]+)|
                           ([0-9]+[^[:alnum:]][0-9]+[a-z]+)|
-                          ([0-9]+[^[:alnum:]][0-9]+[^[:alnum:]][0-9]+[0-9]+[a-z]+))/ix .freeze
+                          ([0-9]+[^[:alnum:]][0-9]+[^[:alnum:]][0-9]+[0-9]+[a-z]+))/ix.freeze
 
             PROTEIN_REGEX = /p\.(\()?((?<impact>.([a-z]+[0-9]+[a-z]+([^[:alnum:]][0-9]+)?)|
                              ([a-z]+[0-9]+[^[:alnum:]])))/ix .freeze

commit 571005fe6705b665ec678fbb4b4660d08de478d4

Fixed bug introduced in 82d5404

lib/import/colorectal/providers/birmingham/birmingham_handler_colorectal.rb
@@ -65,7 +65,7 @@ module Import
                 process_testresult_cdna_variants(testresult, testreport, genelist,
                                                  genotypes, record, genocolorectal)
               elsif testresult.scan(CHR_VARIANTS_REGEX).size.positive?
-                process_chr_variants(record, testreport, genotypes, genocolorectal)
+                process_chr_variants(record, testresult, testreport, genotypes, genocolorectal)
               elsif testresult.match(/No known pathogenic/i)
                 negativegenes = genelist
                 process_negative_genes(negativegenes, genotypes, genocolorectal)

lib/import/helpers/colorectal/providers/rq3/rq3_helper.rb
@@ -138,7 +138,7 @@ module Import
               end
             end
 
-            def process_chr_variants(record, testreport, genotypes, genocolorectal)
+            def process_chr_variants(record, testresult, testreport, genotypes, genocolorectal)
               if full_screen?(record)
                 if sometimes_tested?(record)
                   genelist = testreport.scan(COLORECTAL_GENES_REGEX).flatten.uniq