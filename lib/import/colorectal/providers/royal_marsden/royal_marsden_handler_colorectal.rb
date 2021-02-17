268caed1c9c56ea49fe23635854b7e7195939aaf
Francesco <francesco.santaniello@phe.gov.uk>
2021-02-17 17:46:56 +0000

    #Updated RPY colorectal importer in order not to parse non-colorectal records

--- lib/import/colorectal/providers/royal_marsden/royal_marsden_handler_colorectal.rb
+++ lib/import/colorectal/providers/royal_marsden/royal_marsden_handler_colorectal.rb
@@ -63,24 +63,38 @@ module Import
             genocolorectal.add_passthrough_fields(record.mapped_fields,
                                                   record.raw_fields,
                                                   PASS_THROUGH_FIELDS_COLO)
-            process_gene(genocolorectal, record)
+            # process_gene(genocolorectal, record)
             process_varpathclass(genocolorectal, record)
             process_teststatus(genocolorectal, record)
             process_variant(genocolorectal, record)
             process_large_deldup(genocolorectal, record)
             process_test_scope(genocolorectal, record)
             process_test_type(genocolorectal, record)
-            @persister.integrate_and_store(genocolorectal)
+            # @persister.integrate_and_store(genocolorectal)
+            res = process_gene(genocolorectal, record)# Added by Francesco
+            res.map { |cur_genotype| @persister.integrate_and_store(cur_genotype) }
           end
 
+          # def process_gene(genocolorectal, record)
+          #   genes = record.raw_fields['gene']
+          #   if COLORECTAL_GENES_REGEX.match(genes)
+          #     genocolorectal.add_gene_colorectal($LAST_MATCH_INFO[:colorectal])
+          #     @successful_gene_counter += 1
+          #   end
+          # end
+
           def process_gene(genocolorectal, record)
+            genotypes = []
             genes = record.raw_fields['gene']
             if COLORECTAL_GENES_REGEX.match(genes)
               genocolorectal.add_gene_colorectal($LAST_MATCH_INFO[:colorectal])
               @successful_gene_counter += 1
+              genotypes.append(genocolorectal)
+            else @logger.debug 'NOT A COLORECTAL/LYNCH/MMR RECORD'
             end
+            genotypes
           end
-
+          
           def process_varpathclass(genocolorectal, record)
             varpathclass = record.raw_fields['variantpathclass'].downcase.strip unless record.raw_fields['variantpathclass'].nil?
 
