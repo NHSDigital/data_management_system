fd01dcbc566d0ed64b7b074b5603a8cb81edc1ab
Miles Smith <miles.smith@phe.gov.uk>
2021-02-24 12:54:46 +0000

    # Migrate project comments to comments [#22700]

---
+++ db/migrate/20210208112318_migrate_project_comments_to_comments.rb
@@ -0,0 +1,23 @@
+class MigrateProjectCommentsToComments < ActiveRecord::Migration[6.0]
+  ProjectComment.reset_column_information
+  Comment.reset_column_information
+
+  def up
+    ProjectComment.find_each do |project_comment|
+      commentable = project_comment.project_node || project_comment.project
+
+      commentable.comments.create!(
+        user: project_comment.user,
+        body: project_comment.comment,
+        metadata: {
+          project_comment_id: project_comment.id,
+          tags: [project_comment.comment_type]
+        },
+        created_at: project_comment.created_at,
+        updated_at: project_comment.updated_at
+      )
+    end
+  end
+
+  def down; end
+end
