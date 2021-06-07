804fbf2d3737279d70115373000b502a0995cf28
Francesco <francesco.santaniello@phe.gov.uk>
2021-06-07 10:43:05 +0100

    #Removed deprecated commented code in genotype_brca script

--- lib/import/brca/core/genotype_brca.rb
+++ lib/import/brca/core/genotype_brca.rb
@@ -57,50 +57,6 @@ module Import
           end
         end
 
-        # def add_gene(brca_input)
-        #   case brca_input
-        #   when nil
-        #     @logger.error 'Null input for gene'
-        #   when Integer
-        #     gene_integer_input(brca_input)
-        #   when String
-        #     gene_string_input(brca_input)
-        #   else
-        #     @logger.error "Bad input type given for brca1/2 extraction: #{brca_input}"
-        #   end
-        # end
-
-        # def gene_integer_input(brca_input) # edit so that doesn't guess BRCA genes from Colorectal Genes
-        #   if (1..2).cover? brca_input
-        #     # BRCA 1 and 2 map to gene codes 7 and 8
-        #     @attribute_map['gene'] = brca_input + 6
-        #   elsif (7..8).cover? brca_input
-        #     @attribute_map['gene'] = brca_input
-        #   else
-        #     @logger.error 'Invalid gene reference given to addGene; ' \
-        #     "needs 1 or 2, given: #{brca_input}"
-        #   end
-        # end
-        #
-        # def gene_string_input(brca_input)
-        #   return if brca_input.empty?
-        #
-        #   match_num = brca_input.strip.scan(BRCA_REGEX).size
-        #   if match_num > 1
-        #     @logger.debug 'Bad input string (too many genes) given for brca1/2'\
-        #     " extraction: #{brca_input}"
-        #   elsif match_num.zero?
-        #     @logger.debug 'Bad input string (no detected genes) given for brca1/2'\
-        #     " extraction: #{brca_input}"
-        #   else
-        #     if brca_input.include? '/'
-        #       @logger.debug 'WARNING: string provided for gene extraction contains a'\
-        #       "slash, possible multi-gene error: #{brca_input}"
-        #     end
-        #
-        #     gene_regex_input(brca_input)
-        #   end
-        # end
         def add_gene(brca_input)
           case brca_input
           when Integer
@@ -161,14 +117,6 @@ module Import
           Import::Brca::Core::GenotypeBrca.new(@raw_record, @attribute_map.dup)
         end
 
-        # def gene_regex_input(brca_input)
-        #   case BRCA_REGEX.match(brca_input.strip)
-        #   when nil
-        #     @logger.debug "Bad input string given for brca1/2 extraction: #{brca_input}"
-        #   else
-        #     add_gene($LAST_MATCH_INFO[1].to_i)
-        #   end
-        # end
       end
     end
   end
