fc0c7459001f8160e58f112e260eea30da3372ec
Brian Shand <Brian.Shand@phe.gov.uk>
2021-06-23 15:00:15 +0100

    # Script to extract repseudo_ids from pseudonymised BRCA esources, task #24867

--- db/schema/0000_common_files/repseudo/matched_data_repseudo_linked.sh
+++ db/schema/0000_common_files/repseudo/matched_data_repseudo_linked.sh
@@ -3,7 +3,8 @@
 read -rsp "Enter repseudonymsation salt: " salt_repseudo; echo
 SRC="$1" # Has columns PSEUDO_ID,Patient_NHS_Number,Patient_Date_Of_Birth,n
 DEST="$2"
-ruby repseudo/ndr_repseudonymise.rb \
+DIRNAME="`dirname "$0"`" # Allow script to be run from a different directory
+ruby "$DIRNAME"/ndr_repseudonymise.rb \
      <(sed -e '1s/^PSEUDO_ID,/pseudo_id1,/' "$SRC") \
      "$DEST" "$salt_repseudo" || (
     echo Failed - removing incomplete output file
