8393af11a6d0a6ad78d07f4f679543ec2dc4218e
Francesco <francesco.santaniello@phe.gov.uk>
2021-08-04 14:29:57 +0100

    #Updated genotype_brca, Oxford BRCA provider and coverage test

--- test/lib/import/brca/providers/oxford/oxford_handler_test.rb
+++ test/lib/import/brca/providers/oxford/oxford_handler_test.rb
@@ -12,27 +12,27 @@ class OxfordHandlerTest < ActiveSupport::TestCase
     @logger = Import::Log.get_logger
   end
 
+  test 'assign_servicereportidentifier' do
+    @handler.assign_servicereportidentifier(@genotype, @record)
+    assert_equal '123456', @genotype.attribute_map['servicereportidentifier']
+  end
+
   test 'assign_test_type' do
     @handler.assign_test_type(@genotype, @record)
     assert_equal 1, @genotype.attribute_map['moleculartestingtype']
-    broken_record = build_raw_record('pseudo_id1' => 'bob')
-    broken_record.raw_fields['moleculartestingtype'] = 'Cabbage'
-    @logger.expects(:warn).with('Oxford provided test type: Cabbage; expecteddiagnostic only')
-    @handler.assign_test_type(@genotype, broken_record)
+    pre_symto_record = build_raw_record('pseudo_id1' => 'bob')
+    pre_symto_record.raw_fields['moleculartestingtype'] = 'pre-symptomatic'
+    @handler.assign_test_type(@genotype, pre_symto_record)
+    assert_equal 2, @genotype.attribute_map['moleculartestingtype']
   end
 
   test 'assign_test_scope' do
     @handler.assign_test_scope(@genotype, @record)
     assert_equal 'Full screen BRCA1 and BRCA2', @genotype.attribute_map['genetictestscope']
-  end
-
-  test 'assign_method' do
-    @handler.assign_method(@genotype, @record)
-    assert_equal 17, @genotype.attribute_map['karyotypingmethod']
-    broken_record = build_raw_record('pseudo_id1' => 'bob')
-    broken_record.raw_fields['karyotypingmethod'] = 'Cabbage'
-    @logger.expects(:warn).with('Unknown method: Cabbage; possibly need to update map')
-    @handler.assign_method(@genotype, broken_record)
+    targeted_record = build_raw_record('pseudo_id1' => 'bob')
+    targeted_record.raw_fields['scope / limitations of test'] = 'proband'
+    @handler.assign_test_scope(@genotype, targeted_record)
+    assert_equal 'Targeted BRCA mutation test', @genotype.attribute_map['genetictestscope']
   end
 
   test 'process_gene' do
@@ -51,13 +51,36 @@ class OxfordHandlerTest < ActiveSupport::TestCase
     broken_record.raw_fields['sinonym'] = 'Cabbage'
     @logger.expects(:debug).with('FAILED gene parse')
     @handler.process_gene(@genotype, broken_record)
-    atm_record = build_raw_record('pseudo_id1' => 'bob')
-    atm_record.mapped_fields['gene'] = '451'
-    atm_record.raw_fields['gene'] = 'ATM'
-    atm_record.raw_fields['sinonym'] = 'Cabbage'
-    @logger.expects(:debug).with('SUCCESSFUL gene parse for 451')
-    @logger.expects(:debug).with('SUCCESSFUL gene parse for:451')
-    @handler.process_gene(@genotype, atm_record)
+    chek2_record = build_raw_record('pseudo_id1' => 'bob')
+    chek2_record.mapped_fields['gene'] = '865'
+    chek2_record.raw_fields['gene'] = 'CHEK2'
+    chek2_record.raw_fields['sinonym'] = 'Cabbage'
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for 865')
+    @logger.expects(:debug).with('SUCCESSFUL gene parse for:865')
+    @handler.process_gene(@genotype, chek2_record)
+  end
+
+  test 'process_variants' do
+    @logger.expects(:debug).with('SUCCESSFUL cdna change parse for: 7928C>T')
+    @handler.process_variants(@genotype, @record)
+    assert_equal 'c.7928C>T', @genotype.attribute_map['codingdnasequencechange']
+    assert_equal 2, @genotype.attribute_map['teststatus']
+    exon_variant_record = build_raw_record('pseudo_id1' => 'bob')
+    exon_variant_record.mapped_fields['codingdnasequencechange'] = 'Deletion of exon 12-24'
+    @handler.process_variants(@genotype, exon_variant_record)
+    assert_equal '12-24', @genotype.attribute_map['exonintroncodonnumber']
+    assert_equal 10, @genotype.attribute_map['sequencevarianttype']
+    assert_equal 2, @genotype.attribute_map['teststatus']
+    normal_record = build_raw_record('pseudo_id1' => 'bob')
+    normal_record.raw_fields['codingdnasequencechange'] = 'N/A'
+    normal_record.mapped_fields['codingdnasequencechange'] = 'N/A'
+    @handler.process_variants(@genotype, normal_record)
+    assert_equal 1, @genotype.attribute_map['teststatus']
+    exemptions_record = build_raw_record('pseudo_id1' => 'bob')
+    exemptions_record.mapped_fields['codingdnasequencechange'] = 'c.[-835C>T]+[=]'
+    @handler.process_variants(@genotype, exemptions_record)
+    assert_equal 'c.-835C>T', @genotype.attribute_map['codingdnasequencechange']
+    assert_equal 2, @genotype.attribute_map['teststatus']
   end
 
   test 'process_protein_impact' do
@@ -77,11 +100,6 @@ class OxfordHandlerTest < ActiveSupport::TestCase
     broken_record.raw_fields['genomicchange'] = 'Cabbage'
     @logger.expects(:warn).with('Could not process, so adding raw genomic change: Cabbage')
     @handler.assign_genomic_change(@genotype, broken_record)
-  end
-
-  test 'assign_servicereportidentifier' do
-    @handler.assign_servicereportidentifier(@genotype, @record)
-    assert_equal '123456', @genotype.attribute_map['servicereportidentifier']
   end
 
   private
@@ -111,7 +129,7 @@ class OxfordHandlerTest < ActiveSupport::TestCase
       gene: '8',
       referencetranscriptid: 'NM_000059.3',
       genomicchange: 'Chr13.hg19: g.32936782',
-      codingdnasequencechange: 'c.[7928C\u003eT]+[=]',
+      codingdnasequencechange: 'c.[7928C>T]+[=]',
       proteinimpact: 'p.[Ala2643Val]+[=]',
       variantpathclass: '3',
       age: 999 }.to_json
