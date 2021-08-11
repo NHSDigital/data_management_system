bc21c145e5cd4f1b4bad8b301129ce217edf5d89
Joe <joseph.catling@phe.gov.uk>
2021-08-11 13:31:24 +0100

    initial commit

--- app/models/project_dataset_level.rb
+++ app/models/project_dataset_level.rb
@@ -4,7 +4,9 @@ class ProjectDatasetLevel < ApplicationRecord
   delegate :project, to: :project_dataset
   before_update :set_decided_at_to_nil
   after_update :notify_cas_approved_change
+  after_create :set_expiry_date_to_one_year
 
+  validate :expiry_date_must_be_present_for_level_one_or_extra_datasets
   validates :access_level_id, uniqueness: { scope: :project_dataset_id }
 
   def notify_cas_approved_change
@@ -21,6 +23,23 @@ class ProjectDatasetLevel < ApplicationRecord
     ).deliver_later
 
     approved_change_to_user
+  end
+
+  def set_expiry_date_to_one_year
+    return unless project.cas?
+    return unless project_dataset.dataset.cas_defaults?
+    return unless [2, 3].include? access_level_id
+
+    update_column(:expiry_date, 1.year.from_now)
+  end
+
+  def expiry_date_must_be_present_for_level_one_or_extra_datasets
+    return unless project.cas?
+    return unless selected == true
+    return if project_dataset.dataset.cas_extras? || [2, 3].include?(access_level_id)
+
+    errors.add :project_datasets, 'expiry date must be present for all selected extra datasets ' \
+                                  'and any selected level 1 default datasets'
   end
 
   def readable_approved_status
