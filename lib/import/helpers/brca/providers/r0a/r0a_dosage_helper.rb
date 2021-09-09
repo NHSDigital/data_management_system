61418e6bce567fa75967bf4b11e9d3db2bd4bee2
Francesco <francesco.santaniello@phe.gov.uk>
2021-09-09 13:54:04 +0100

    #Added dosage-nondosage specific helper scripts

---
+++ lib/import/helpers/brca/providers/r0a/r0a_dosage_helper.rb
@@ -0,0 +1,135 @@
+module Import
+  module Helpers
+    module Brca
+      module Providers
+        module R0a
+          # Helper methods for R0A germline extractor
+          module R0aDosageHelper
+            include Import::Helpers::Brca::Providers::R0a::R0aConstants
+
+            def split_multiplegenes_dosage_map
+              @dosage_record_map[:exon].each.with_index do |exon, index|
+                if exon.scan(BRCA_GENES_REGEX).size > 1
+                  @dosage_record_map[:exon][index] =
+                    @dosage_record_map[:exon][index].scan(BRCA_GENES_REGEX).flatten.each do |gene|
+                      gene.concat('_MLPA')
+                    end
+                  @dosage_record_map[:genotype][index] =
+                    edit_dosage_genotype_field(exon, index)
+                  @dosage_record_map[:genotype2][index] =
+                    edit_dosage_genotype2_field(exon, index)
+                end
+              end
+              @dosage_record_map[:exon] = @dosage_record_map[:exon].flatten
+              @dosage_record_map[:genotype] = @dosage_record_map[:genotype].flatten
+              @dosage_record_map[:genotype2] = @dosage_record_map[:genotype2].flatten
+            end
+
+            def edit_dosage_genotype_field(exon, index)
+            case @dosage_record_map[:genotype][index]
+              when 'Normal'
+                @dosage_record_map[:genotype][index] =
+                  ['Normal'] * exon.scan(BRCA_GENES_REGEX).size
+                @dosage_record_map[:genotype][index] =
+                  @dosage_record_map[:genotype][index].flatten
+              when 'BRCA1 Normal, BRCA2 Normal'
+                @dosage_record_map[:genotype][index] = ['NGS Normal'] * 2
+                @dosage_record_map[:genotype][index] =
+                  @dosage_record_map[:genotype][index].flatten
+              end
+            end
+
+            def edit_dosage_genotype2_field(exon, index)
+              if !@dosage_record_map[:genotype2][index].nil? &&
+                 @dosage_record_map[:genotype2][index].empty?
+                @dosage_record_map[:genotype2][index] = ['MLPA Normal'] * 2
+                @dosage_record_map[:genotype2][index] =
+                  @dosage_record_map[:genotype2][index].flatten
+              elsif !@dosage_record_map[:genotype2][index].nil? &&
+                    @dosage_record_map[:genotype2][index].scan(/100% coverage at 100X/).size.positive?
+                @dosage_record_map[:genotype2][index] = ['NGS Normal'] * 2
+                @dosage_record_map[:genotype2][index] =
+                  @dosage_record_map[:genotype2][index].flatten
+              end
+            end
+
+            def dosage_test?
+              @dosage_record_map[:moleculartestingtype].uniq.join.scan(/dosage/i).size.positive?
+            end
+
+            def process_dosage_test_exons(genes)
+              @dosage_record_map[:exon].map do |exons|
+                if exons.scan(BRCA_GENES_REGEX).count.positive? # && mlpa?(exons)
+                  exons.scan(BRCA_GENES_REGEX).flatten.each { |gene| genes.append(gene) }
+                #                  #binding.pry
+                else
+                  genes.append('No Gene')
+                end
+              end
+            end
+
+            def tests_from_dosage_record(genes)
+              return if genes.nil?
+
+              genes.zip(@dosage_record_map[:genotype],
+                        @dosage_record_map[:genotype2]).uniq
+            end
+
+            def process_grouped_dosage_tests(grouped_tests, genotype, genotypes)
+              if (@non_dosage_record_map[:moleculartestingtype].uniq & DO_NOT_IMPORT).empty?
+                grouped_tests.compact.select do |gene, genetic_info|
+                  process_dosage_gene(gene, genetic_info, genotype, genotypes)
+                end
+              else
+                @logger.debug('Nothing to do')
+              end
+            end
+
+            def process_dosage_gene(gene, genetic_info, genotype, genotypes)
+              if !brca_gene_match?(genetic_info)
+                if gene == 'No Gene'
+                  @logger.debug("Nothing to do for #{gene} and #{genetic_info}")
+                elsif !cdna_match?(genetic_info) &&
+                      !exon_match?(genetic_info) &&
+                      normal?(genetic_info)
+                  genotype_dup = genotype.dup
+                  process_non_cdna_normal(gene, genetic_info, genotype, genotypes)
+                elsif cdna_match?(genetic_info)
+                  process_non_dosage_cdna(gene, genetic_info, genotype, genotypes)
+                  @logger.debug("IDENTIFIED #{gene}, #{cdna_from(genetic_info)} from #{genetic_info}")
+                elsif genetic_info.join(',').match(EXON_LOCATION_REGEX) && exon_match?(genetic_info)
+                  process_brca_gene_and_exon_match(genotype, gene, genetic_info, genotypes)
+                elsif !genetic_info.join(',').match(EXON_LOCATION_REGEX) && exon_match?(genetic_info)
+                  case genetic_info.join(',')
+                  when /normal/i, /evidence/i
+                    process_non_cdna_normal(gene, genetic_info, genotype, genotypes)
+                  when /control/i
+                    @logger.debug("IDENTIFIED FALSE POSITIVE #{gene} #{genetic_info}, skipping entry")
+                  end
+                elsif genetic_info.join(',').empty?
+                  @logger.debug("IDENTIFIED FALSE POSITIVE #{gene} #{genetic_info}, skipping entry")
+                elsif genetic_info.join(',').match(/evidence/i)
+                  process_non_cdna_normal(gene, genetic_info, genotype, genotypes)
+                end
+              elsif brca_gene_match?(genetic_info) && !exon_match?(genetic_info)
+                if genetic_info.join(',').match(BRCA_GENES_REGEX)[:brca] == gene
+                  genotype_dup = genotype.dup
+                  add_gene_and_status_to(genotype_dup, gene, 1, genotypes)
+                else
+                  @logger.debug("IDENTIFIED FALSE POSITIVE #{gene} #{genetic_info}, skipping entry")
+                end
+              elsif brca_gene_match?(genetic_info) && exon_match?(genetic_info)
+                if genetic_info.join(',').match(BRCA_GENES_REGEX)[:brca] == gene
+                  process_brca_gene_and_exon_match(genotype, gene, genetic_info, genotypes)
+                else
+                  @logger.debug("IDENTIFIED FALSE POSITIVE #{gene} #{genetic_info}, skipping entry")
+                end
+              end
+            end
+
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/import/helpers/brca/providers/r0a/r0a_nondosage_helper.rb
@@ -0,0 +1,159 @@
+module Import
+  module Helpers
+    module Brca
+      module Providers
+        module R0a
+          # Helper methods for R0A germline extractor
+          module R0aNondosageHelper
+            include Import::Helpers::Brca::Providers::R0a::R0aConstants
+            
+            def non_dosage_test?
+              @non_dosage_record_map[:moleculartestingtype].uniq.join.scan(/dosage/i).size.zero?
+            end
+
+            def split_multiplegenes_nondosage_map
+              @non_dosage_record_map[:exon].each.with_index do |exon, index|
+                next unless exon.scan(BRCA_GENES_REGEX).size.positive?
+
+                if exon.scan(BRCA_GENES_REGEX).uniq.size > 1
+                  @non_dosage_record_map[:exon][index] =
+                    @non_dosage_record_map[:exon][index].scan(BRCA_GENES_REGEX).uniq
+                  @non_dosage_record_map[:exon][index].flatten
+                  @non_dosage_record_map[:genotype][index] = 
+                    edit_nondosage_genotype_field(exon, index)
+                  @non_dosage_record_map[:genotype2][index] = 
+                    edit_nondosage_genotype2_field(exon, index)
+                end
+              end
+              @non_dosage_record_map[:exon] = @non_dosage_record_map[:exon].flatten
+              @non_dosage_record_map[:genotype] = @non_dosage_record_map[:genotype].flatten
+              @non_dosage_record_map[:genotype2] = @non_dosage_record_map[:genotype2].flatten
+            end
+
+            def edit_nondosage_genotype_field(exon, index)
+              if @non_dosage_record_map[:genotype][index] == 'BRCA1 Normal, BRCA2 Normal'
+                @non_dosage_record_map[:genotype][index] = ['NGS Normal'] * 2
+                @non_dosage_record_map[:genotype][index] =
+                  @non_dosage_record_map[:genotype][index].flatten
+              elsif @non_dosage_record_map[:genotype][index].scan(/Normal, /i).size.positive?
+                @non_dosage_record_map[:genotype][index] =
+                  @non_dosage_record_map[:genotype][index] = ['NGS Normal'] * 2
+              elsif @non_dosage_record_map[:genotype][index].scan(/,.+Normal/i).size.positive?
+                @non_dosage_record_map[:genotype][index] =
+                  @non_dosage_record_map[:genotype][index] = ['NGS Normal'] * 2
+              elsif @non_dosage_record_map[:genotype][index] == 'Normal'
+                @non_dosage_record_map[:genotype][index] =
+                  ['Normal'] * exon.scan(BRCA_GENES_REGEX).uniq.size
+                @non_dosage_record_map[:genotype][index] =
+                  @non_dosage_record_map[:genotype][index].flatten
+              else
+                @non_dosage_record_map[:genotype][index] =
+                  @non_dosage_record_map[:genotype][index]
+              end
+              @non_dosage_record_map[:genotype] = @non_dosage_record_map[:genotype].flatten
+            end
+
+            def edit_nondosage_genotype2_field(exon, index)
+              if !@non_dosage_record_map[:genotype2][index].nil? &&
+                 @non_dosage_record_map[:genotype2][index].scan(/coverage at 100X/).size.positive?
+                @non_dosage_record_map[:genotype2][index] = ['NGS Normal'] * 2
+                @non_dosage_record_map[:genotype2][index] =
+                  @non_dosage_record_map[:genotype2][index].flatten
+              elsif !@non_dosage_record_map[:genotype2][index].nil? &&
+                    @non_dosage_record_map[:genotype2][index].empty?
+                @non_dosage_record_map[:genotype2][index] = ['MLPA Normal'] * 2
+                @non_dosage_record_map[:genotype2][index] =
+                  @non_dosage_record_map[:genotype2][index].flatten
+              elsif @non_dosage_record_map[:genotype2][index].nil? &&
+                    @non_dosage_record_map[:genotype][index].is_a?(String) &&
+                    @non_dosage_record_map[:genotype][index].scan(/MSH2/).size.positive?
+                @non_dosage_record_map[:genotype2][index] =
+                  [''] * exon.scan(BRCA_GENES_REGEX).size
+                @non_dosage_record_map[:genotype2][index] =
+                  @non_dosage_record_map[:genotype2][index].flatten
+              elsif @non_dosage_record_map[:genotype2][index] == 'Normal' ||
+                    @non_dosage_record_map[:genotype2][index].nil? ||
+                    @non_dosage_record_map[:genotype2][index] == 'Fail'
+                @non_dosage_record_map[:genotype2][index] =
+                  ['Normal'] * exon.scan(BRCA_GENES_REGEX).uniq.size
+                @non_dosage_record_map[:genotype2][index] =
+                  @non_dosage_record_map[:genotype2][index].flatten
+              end
+            end
+
+            def process_grouped_non_dosage_tests(grouped_tests, genotype, genotypes)
+              if (@non_dosage_record_map[:moleculartestingtype].uniq & DO_NOT_IMPORT).empty?
+                grouped_tests.each do |gene, genetic_info|
+                  # next unless MOLTEST_MAP[selected_genes].include? gene
+                  if gene == 'No Gene'
+                    @logger.debug("Nothing to do for #{gene} and #{genetic_info}")
+                  elsif cdna_match?(genetic_info)
+                    process_non_dosage_cdna(gene, genetic_info, genotype, genotypes)
+                  elsif genetic_info.join(',').match(EXON_LOCATION_REGEX) &&
+                        exon_match?(genetic_info)
+                    process_brca_gene_and_exon_match(genotype, gene, genetic_info, genotypes)
+                  elsif !cdna_match?(genetic_info) &&
+                        !exon_match?(genetic_info) &&
+                        normal?(genetic_info)
+                    process_non_cdna_normal(gene, genetic_info, genotype, genotypes)
+                  elsif !cdna_match?(genetic_info) &&
+                        !exon_match?(genetic_info) &&
+                        !normal?(genetic_info) &&
+                        fail?(genetic_info)
+                    process_non_cdna_fail(gene, genetic_info, genotype, genotypes)
+                  end
+                end
+              else
+                @logger.debug('Nothing to do')
+              end
+            end
+
+            def process_non_dosage_cdna(gene, genetic_info, genotype, genotypes)
+              genotype_dup = genotype.dup
+              process_non_brca_genes(genotype_dup, gene, genetic_info, genotypes,
+                                     genotype)
+            end
+
+            def tests_from_non_dosage_record(genes)
+              return if genes.nil?
+
+              genes.zip(@non_dosage_record_map[:genotype],
+                        @non_dosage_record_map[:genotype2]).uniq
+            end
+
+            def process_non_dosage_test_exons(genes)
+              @non_dosage_record_map[:exon].each do |exons|
+                if exons =~ BRCA_GENES_REGEX
+                  genes.append(BRCA_GENES_REGEX.match(exons.upcase)[:brca])
+                else
+                  genes.append('No Gene')
+                end
+              end
+            end
+
+            # def restructure_oddlynamed_nondosage_exons(dosage_nondosage)
+            #   dosage_nondosage[:exon] = dosage_nondosage[:exon].flatten
+            #   dosage_nondosage[:exon] =
+            #     dosage_nondosage[:exon].map do |exons|
+            #       exons.gsub(/BRCA2|B2|BR2|P045|P077/, 'BRCA2')
+            #     end
+            #   dosage_nondosage[:exon] =
+            #     dosage_nondosage[:exon].map do |exons|
+            #       exons.gsub(/BRCA1|B1|BR1|P002|P002B|P087/, 'BRCA1')
+            #     end
+            #   dosage_nondosage[:exon] =
+            #     dosage_nondosage[:exon].map do |exons|
+            #       exons.gsub(/ATM|P041|P042/, 'ATM')
+            #     end
+            #   dosage_nondosage[:exon] =
+            #     dosage_nondosage[:exon].map do |exons|
+            #       exons.gsub(/CHEK2|CKEK2|P190/, 'CHEK2')
+            #     end
+            # end
+
+          end
+        end
+      end
+    end
+  end
+end
