2df62771e2ef82069d48ed7336903f219a2cc0a1
FrancescoNea <francesconea@gmail.com>
2021-09-13 20:21:28 +0100

    Updated BRCA handler script after boyscouting

--- lib/import/brca/core/brca_handler_mapping.rb
+++ lib/import/brca/core/brca_handler_mapping.rb
@@ -21,7 +21,7 @@ module Import
           'RPY' => Import::Brca::Providers::RoyalMarsden::RoyalMarsdenHandler,
           'R1K' => Import::Brca::Providers::LondonKgc::LondonKgcHandler,
           'RQ3' => Import::Brca::Providers::Birmingham::BirminghamHandlerNewformat,
-          'RP4' => Import::Brca::Providers::LondonGosh::LondonGoshHandler,
+          'RP4' => Import::Brca::Providers::LondonGosh::LondonGoshHandler
         }.freeze
 
         def self.get_handler(provider_code)
