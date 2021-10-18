ab0a85dbc5f6766f7f94018db982e6b118dd7ce3
Brian Shand <Brian.Shand@phe.gov.uk>
2021-10-18 12:05:35 +0100

    # Script for provisional 2021 death extracts, task #14947

--- lib/export/scripts/extract_provisional_deaths.sh
+++ lib/export/scripts/extract_provisional_deaths.sh
@@ -9,7 +9,7 @@ if [ "$EXPORT_KLASS" == "Export::CancerDeathWeekly" ]; then
   # FILTER_PREFIX=rd # Only records for Rare Diseases (for example)
   # FILTER_PREFIX='' # All records
   if [ -z "$FILTER_PREFIX" ]; then
-      read -rp "Enter FILTER_PREFIX (blank for all records, e.g. rb_ for Rare Disease only): " FILTER_PREFIX
+      read -rp "Enter FILTER_PREFIX (blank for all records, e.g. rd_ for Rare Disease only): " FILTER_PREFIX
       export FILTER_PREFIX
   fi
 	FILTER_ALL="${FILTER_PREFIX}_all"
@@ -27,20 +27,26 @@ else
   DOR_COLUMN="DOR"
 fi
 
+if [ -z "$YEAR" ]; then
+    read -rp "Enter YEAR, e.g. 2021: " YEAR
+fi
+
 # Prompt for MBIS_KEK
 if [ -z "$MBIS_KEK" ]; then
     read -rsp 'MBIS_KEK: ' MBIS_KEK; echo
     export MBIS_KEK
 fi
 
-YEAR=2020
+YEAR_YY=`echo $YEAR | cut -c3-4` # last 2 digits e.g. 21 for YEAR=2021
+NEXT_YEAR_YY=`echo $(($YEAR + 1))|cut -c3-4` # last 2 digits of following year, e.g. 22
+TODAY=`date +%Y%m%d`
+
 ROLLUP_FILE=""
 # ROLLUP_FILE="deaths/MBIS_DEATHS_2018_REFRESH_20190118.txt"O
-OUTFILE=${YEAR}_provisional_deaths.csv
-TMPFILE=${YEAR}_death_tmp.csv
+OUTFILE=${YEAR}_provisional_deaths_${TODAY}.csv
 
 if [ -z "$ROLLUP_FILE" ]; then
-    # No roll-up file actually required for 2019 deaths, just using the weekly files
+    # No roll-up file actually required for 2019 deaths onwards, just using the weekly files
     rm -f private/mbis_data/extracts/"$OUTFILE"
 else
     echo "$ROLLUP_FILE" | \
@@ -51,17 +57,21 @@ else
         done
 fi
 
-bundle exec rails runner -e production 'puts EBatch.where(e_type: "PSDEATH").order(:e_batchid).pluck(:original_filename).select{|s|s =~ /^deaths\/MBISWEEKLY_Deaths_D(20[0-9][0-9]|2101)[0-9]{2}.txt$/}' | \
+bundle exec rails runner -e production "puts EBatch.where(e_type: 'PSDEATH').order(:e_batchid).pluck(:original_filename).select{|s|s =~ /^deaths\/MBISWEEKLY_Deaths_D($YEAR_YY[0-9][0-9]|${NEXT_YEAR_YY}01)[0-9]{2}.txt$/}" | \
 while read BASE; do
   echo Exporting $BASE:
+  TMPFILE_FULL=`mktemp private/mbis_data/extracts/${YEAR}_death_tmp.XXXXXX`
+  TMPFILE=`basename "$TMPFILE_FULL"`
   bundle exec rake export:death fname="$TMPFILE" original_filename="$BASE" klass="$EXPORT_KLASS" filter="$FILTER_NEW" RAILS_ENV=production
 	if [ ! -e private/mbis_data/extracts/"$OUTFILE" ]; then
 			# Create header row, if no roll-up file available
-			head -1 private/mbis_data/extracts/"$TMPFILE" > private/mbis_data/extracts/"$OUTFILE"
+			head -1 "$TMPFILE_FULL" > private/mbis_data/extracts/"$OUTFILE"
 	fi
-	tail -n+2 private/mbis_data/extracts/"$TMPFILE" >> private/mbis_data/extracts/"$OUTFILE"
-  mv private/mbis_data/extracts/"$TMPFILE" `mktemp private/mbis_data/extracts/${YEAR}_death_testing.XXXXX`
+	tail -n+2 "$TMPFILE_FULL" >> private/mbis_data/extracts/"$OUTFILE"
+  # For testing: instead preserve temporary files
+  # mv "$TMPFILE_FULL" `mktemp private/mbis_data/extracts/${YEAR}_death_testing.XXXXX`
+  rm -f "$TMPFILE_FULL"
 done
 # Minimalist ruby-based csvcut, based on DOR in ${YEAR}
-lib/export/scripts/csvcut_dor.sh ${DOR_COLUMN} ${YEAR} < private/mbis_data/extracts/"$OUTFILE" > private/mbis_data/extracts/${YEAR}_provisional_deaths_DOR${YEAR}.csv
+lib/export/scripts/csvcut_dor.sh ${DOR_COLUMN} ${YEAR} < private/mbis_data/extracts/"$OUTFILE" > private/mbis_data/extracts/${YEAR}_provisional_deaths_DOR${YEAR}_${TODAY}.csv
 echo Done
