f94cbc310c6afd031048cdc0a98c5958f272bbfd
Francesco <francesco.santaniello@phe.gov.uk>
2022-06-29 13:45:28 +0100

    #Started editing RNZ CRC test after main extractor restructuring

--- test/lib/import/colorectal/providers/salisbury/salisbury_handler_colorectal_test.rb
+++ test/lib/import/colorectal/providers/salisbury/salisbury_handler_colorectal_test.rb
@@ -8,10 +8,9 @@ class SalisburyHandlerColorectalTest < ActiveSupport::TestCase
   def setup
     @record   = build_raw_record('pseudo_id1' => 'bob')
     @genotype = Import::Colorectal::Core::Genocolorectal.new(@record)
-    # TODO: Fully qualify CambridgeHandler in cambridge_handler.rb
     @importer_stdout, @importer_stderr = capture_io do
       @handler = Import::Colorectal::Providers::Salisbury::SalisburyHandlerColorectal.new(EBatch.new)
-    end
+      end
     @logger = Import::Log.get_logger
   end
 
@@ -28,64 +27,79 @@ class SalisburyHandlerColorectalTest < ActiveSupport::TestCase
     Import::Brca::Core::RawRecord.new(default_options.merge!(options))
   end
 
-  test 'extract_variant' do
-    @handler.extract_variant(@record.raw_fields['genotype'], @genotype)
-    assert_equal 'c.1621A>C', @genotype.attribute_map['codingdnasequencechange']
-    broken_record = build_raw_record('pseudo_id1' => 'bob')
-    broken_record.raw_fields['genotype'] = 'Cabbage'
-    @logger.expects(:warn).with('Cannot extract gene location from raw test: Cabbage')
-    @handler.extract_variant(broken_record.raw_fields['genotype'], @genotype)
-    nil_record = build_raw_record('pseudo_id1' => 'bob')
-    nil_record.raw_fields['genotype'] = ''
-    @handler.extract_variant(nil_record.raw_fields['genotype'], @genotype)
-    assert_equal 1, @genotype.attribute_map['teststatus']
-  end
-
-  test 'add_colorectal_from_raw_test' do
-    @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH6')
-    @logger.expects(:debug).with('SUCCESSFUL gene change parse for: MSH6')
-    @handler.add_colorectal_from_raw_test(@genotype, @record)
-    assert_equal 2808, @genotype.attribute_map['gene']
-    multigene_record = build_raw_record('pseudo_id1' => 'bob')
-    multigene_record.raw_fields['test'] = 'MLH1 and MSH2 test'
-    @logger.expects(:error).with('Multiple genes detected in input string: MLH1 and MSH2 test;')
+  test 'process_multiple_tested_genes' do
+    multigene_normal_record = build_raw_record('pseudo_id1' => 'bob')
+    multigene_normal_record.raw_fields['test'] = 'MLH1 and MSH2 test'
+    multigene_normal_record.raw_fields['status'] = 'Normal'
+    multigene_normal_record.raw_fields['genotype'] = ''
     @logger.expects(:debug).with('SUCCESSFUL gene parse for MLH1')
     @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH2')
-    @handler.add_colorectal_from_raw_test(@genotype, multigene_record)
-    cabbage_record = build_raw_record('pseudo_id1' => 'bob')
-    cabbage_record.raw_fields['test'] = 'COO-COO!'
-    @logger.expects(:debug).with('FAILED cdna channge parse for  COO-COO!')
-    @handler.add_colorectal_from_raw_test(@genotype, cabbage_record)
+    res = @handler.add_colorectal_from_raw_test(@genotype, multigene_normal_record)
+    assert_equal 2, res.size
+    assert_equal 2744, res[0].attribute_map['gene']
+    assert_equal 1, res[0].attribute_map['teststatus']
+    assert_equal 2804, res[1].attribute_map['gene']
+    assert_equal 1, res[1].attribute_map['teststatus']
   end
 
-  test 'extract_teststatus' do
-    @logger.expects(:debug).with('POSITIVE status for : Likely pathogenic')
-    @handler.extract_teststatus(@genotype, @record)
-    assert_equal 2, @genotype.attribute_map['teststatus']
+  # test 'extract_variant' do
+  #   @handler.extract_variant(@record.raw_fields['genotype'], @genotype)
+  #   assert_equal 'c.1621A>C', @genotype.attribute_map['codingdnasequencechange']
+  #   broken_record = build_raw_record('pseudo_id1' => 'bob')
+  #   broken_record.raw_fields['genotype'] = 'Cabbage'
+  #   @logger.expects(:warn).with('Cannot extract gene location from raw test: Cabbage')
+  #   @handler.extract_variant(broken_record.raw_fields['genotype'], @genotype)
+  #   nil_record = build_raw_record('pseudo_id1' => 'bob')
+  #   nil_record.raw_fields['genotype'] = ''
+  #   @handler.extract_variant(nil_record.raw_fields['genotype'], @genotype)
+  #   assert_equal 1, @genotype.attribute_map['teststatus']
+  # end
 
-    failed_record = build_raw_record('pseudo_id1' => 'bob')
-    failed_record.raw_fields['status'] = 'Failed'
-    @logger.expects(:debug).with('FAILED status for : Failed')
-    @handler.extract_teststatus(@genotype, failed_record)
-    assert_equal 9, @genotype.attribute_map['teststatus']
+  # test 'add_colorectal_from_raw_test' do
+  #   @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH6')
+  #   # @logger.expects(:debug).with('SUCCESSFUL gene change parse for: MSH6')
+  #   @handler.add_colorectal_from_raw_test(@genotype, @record)
+  #   assert_equal 2808, @genotype.attribute_map['gene']
+  #   multigene_record = build_raw_record('pseudo_id1' => 'bob')
+  #   multigene_record.raw_fields['test'] = 'MLH1 and MSH2 test'
+  #   @logger.expects(:error).with('Multiple genes detected in input string: MLH1 and MSH2 test;')
+  #   @logger.expects(:debug).with('SUCCESSFUL gene parse for MLH1')
+  #   @logger.expects(:debug).with('SUCCESSFUL gene parse for MSH2')
+  #   @handler.add_colorectal_from_raw_test(@genotype, multigene_record)
+  #   cabbage_record = build_raw_record('pseudo_id1' => 'bob')
+  #   cabbage_record.raw_fields['test'] = 'COO-COO!'
+  #   @logger.expects(:debug).with('FAILED cdna channge parse for  COO-COO!')
+  #   @handler.add_colorectal_from_raw_test(@genotype, cabbage_record)
+  # end
 
-    normal_record = build_raw_record('pseudo_id1' => 'bob')
-    normal_record.raw_fields['status'] = 'Normal'
-    @logger.expects(:debug).with('POSITIVE status for : Normal')
-    @handler.extract_teststatus(@genotype, normal_record)
-    assert_equal 1, @genotype.attribute_map['teststatus']
-
-    nomut_record = build_raw_record('pseudo_id1' => 'bob')
-    nomut_record.raw_fields['status'] = 'No mutation detected'
-    @logger.expects(:debug).with('POSITIVE status for : No mutation detected')
-    @handler.extract_teststatus(@genotype, nomut_record)
-    assert_equal 1, @genotype.attribute_map['teststatus']
-
-    cabbage_record = build_raw_record('pseudo_id1' => 'bob')
-    cabbage_record.raw_fields['status'] = 'Cabbage'
-    @logger.expects(:debug).with('Cannot determine test status for : Cabbage')
-    @handler.extract_teststatus(@genotype, cabbage_record)
-  end
+  # test 'extract_teststatus' do
+  #   @logger.expects(:debug).with('POSITIVE status for : Likely pathogenic')
+  #   @handler.extract_teststatus(@genotype, @record)
+  #   assert_equal 2, @genotype.attribute_map['teststatus']
+  #
+  #   failed_record = build_raw_record('pseudo_id1' => 'bob')
+  #   failed_record.raw_fields['status'] = 'Failed'
+  #   @logger.expects(:debug).with('FAILED status for : Failed')
+  #   @handler.extract_teststatus(@genotype, failed_record)
+  #   assert_equal 9, @genotype.attribute_map['teststatus']
+  #
+  #   normal_record = build_raw_record('pseudo_id1' => 'bob')
+  #   normal_record.raw_fields['status'] = 'Normal'
+  #   @logger.expects(:debug).with('POSITIVE status for : Normal')
+  #   @handler.extract_teststatus(@genotype, normal_record)
+  #   assert_equal 1, @genotype.attribute_map['teststatus']
+  #
+  #   nomut_record = build_raw_record('pseudo_id1' => 'bob')
+  #   nomut_record.raw_fields['status'] = 'No mutation detected'
+  #   @logger.expects(:debug).with('POSITIVE status for : No mutation detected')
+  #   @handler.extract_teststatus(@genotype, nomut_record)
+  #   assert_equal 1, @genotype.attribute_map['teststatus']
+  #
+  #   cabbage_record = build_raw_record('pseudo_id1' => 'bob')
+  #   cabbage_record.raw_fields['status'] = 'Cabbage'
+  #   @logger.expects(:debug).with('Cannot determine test status for : Cabbage')
+  #   @handler.extract_teststatus(@genotype, cabbage_record)
+  # end
 
   def clinical_json
     { sex: '1',
