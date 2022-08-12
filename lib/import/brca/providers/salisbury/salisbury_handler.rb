056e9286c48833a0649ed5fbf0dab560e13e30f1
shilpigoel <shilpi.goel@phe.gov.uk>
2022-08-12 11:13:22 +0100

    Review comments changes

--- lib/import/brca/providers/salisbury/salisbury_handler.rb
+++ lib/import/brca/providers/salisbury/salisbury_handler.rb
@@ -17,8 +17,8 @@ module Import
             process_molecular_testing(genotype, record)
             add_organisationcode_testresult(genotype)
             extract_teststatus(genotype, record)
-            res = process_variant_record(genotype, record)
-            res.each { |cur_genotype| @persister.integrate_and_store(cur_genotype) }
+            results = process_variant_record(genotype, record)
+            results.each { |cur_genotype| @persister.integrate_and_store(cur_genotype) }
           end
 
           def process_molecular_testing(genotype, record)
@@ -34,7 +34,7 @@ module Import
 
           def extract_teststatus(genotype, record)
             status = record.raw_fields['status']&.downcase
-            geno_str = record.raw_fields['genotype']
+            geno_string = record.raw_fields['genotype']
             if POSITIVE_STATUS.include?(status)
               genotype.add_status(:positive)
             elsif NEGATIVE_STATUS.include?(status)
@@ -44,7 +44,7 @@ module Import
             elsif UNKNOWN_STATUS.include?(status)
               genotype.add_status(:unknown)
             elsif GENO_DEPEND_STATUS.include?(status)
-              teststatus = geno_str.blank? ? :negative : :positive
+              teststatus = geno_string.blank? ? :negative : :positive
               genotype.add_status(teststatus)
             end
             @logger.debug "#{genotype.attribute_map['teststatus']} status for : #{status}"
@@ -53,15 +53,15 @@ module Import
           def process_variant_record(genotype, record)
             genotypes = []
             variant = record.raw_fields['genotype']
-            test_str = record.raw_fields['test']
-            gene = extract_gene(test_str, variant, record)
+            test_string = record.raw_fields['test']
+            gene = extract_gene(test_string, variant, record)
             genotype.add_gene(gene[0])
-            if test_str.scan(CONFIRM_SEQ_NGS).size.positive?
+            if test_string.scan(CONFIRM_SEQ_NGS).size.positive?
               add_fs_negative_gene(gene, genotype, genotypes)
             end
 
-            process_variants(genotype, variant) if positive_rec?(genotype) && variant.present?
-            unless test_str.scan(CONFIRM_SEQ_NGS).size.positive? && gene.blank?
+            process_variants(genotype, variant) if positive_record?(genotype) && variant.present?
+            unless test_string.scan(CONFIRM_SEQ_NGS).size.positive? && gene.blank?
               genotypes.append(genotype)
             end
 
@@ -96,8 +96,8 @@ module Import
             @logger.debug "SUCCESSFUL protein parse for: #{$LAST_MATCH_INFO[:impact]}"
           end
 
-          def add_fs_negative_gene(pos_gene, genotype, genotypes)
-            negative_genes = %w[BRCA1 BRCA2] - pos_gene
+          def add_fs_negative_gene(positive_gene, genotype, genotypes)
+            negative_genes = %w[BRCA1 BRCA2] - positive_gene
             negative_genes&.each do |brca_gene|
               genotype_dup = genotype.dup
               genotype_dup.add_gene(brca_gene)
@@ -106,16 +106,16 @@ module Import
             end
           end
 
-          def positive_rec?(genotype)
+          def positive_record?(genotype)
             genotype.attribute_map['teststatus'] == 2
           end
 
-          # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity
-          def extract_gene(test_string, geno_str, record)
+          # rubocop:disable Metrics/MethodLength, Metrics/CyclomaticComplexity, Layout/LineLength
+          def extract_gene(test_string, geno_string, record)
             positive_gene = []
             positive_gene << 'BRCA1' if record.raw_fields['servicereportidentifier'] == 'W1715894'
 
-            gene_string = test_string.scan(CONFIRM_SEQ_NGS).size.positive? ? geno_str : test_string
+            gene_string = test_string.scan(CONFIRM_SEQ_NGS).size.positive? ? geno_string : test_string
 
             case gene_string
             when /BRCA1|BC1/i
@@ -135,7 +135,7 @@ module Import
             end
             positive_gene
           end
-          # rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity
+          # rubocop:enable Metrics/MethodLength, Metrics/CyclomaticComplexity, Layout/LineLength
         end
       end
     end
