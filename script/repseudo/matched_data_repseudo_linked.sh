22c41c0b5e2ea1bf7f9423205134592e00ff9c57
Brian Shand <Brian.Shand@phe.gov.uk>
2022-12-05 14:07:43 +0000

    # Repseudonymisation should support environment variable configuration.

--- script/repseudo/matched_data_repseudo_linked.sh
+++ script/repseudo/matched_data_repseudo_linked.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-read -rsp "Enter repseudonymisation salt: " salt_repseudo; echo
+if [ -z "$REPSEUDOSALT" ]; then
+    read -rsp "Enter repseudonymisation salt: " REPSEUDOSALT; echo
+fi
 SRC="$1" # Has columns PSEUDO_ID,Patient_NHS_Number,Patient_Date_Of_Birth,n
 DEST="$2"
 DIRNAME="`dirname "$0"`" # Allow script to be run from a different directory
 ruby "$DIRNAME"/ndr_repseudonymise.rb \
      <(sed -e '1s/^PSEUDO_ID,/pseudo_id1,/' "$SRC") \
-     "$DEST" "$salt_repseudo" || (
+     "$DEST" "$REPSEUDOSALT" || (
     echo Failed - removing incomplete output file
     rm -f "$DEST"
 )
